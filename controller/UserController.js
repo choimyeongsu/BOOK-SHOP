5b06da346ed7c61eb2ca9276bac7d91398e40717
choims <chlaudtn0257@gmail.com>
2024-01-03 01:55:53 +0900

    refactor:users.js 라우트 수정, feat:UserController.js 생성(회원가입,로그인,비밀번호초기화요청,비밀번호초기화 api 로직생성)

---
+++ controller/UserController.js
@@ -0,0 +1,108 @@
+const conn = require('../mariadb'); //db모듈
+const {StatusCodes} = require('http-status-codes');//status code 모듈
+const jwt = require('jsonwebtoken'); //jwt 모듈 
+const crypto = require('crypto'); // crypot 모듈 : 암호화
+const dotenv = require('dotenv'); // dotenv 모듈
+dotenv.config();
+
+const join = (req,res)=>{
+    let {email,password}=req.body;
+    const sql = `INSERT INTO users (email,password,salt) VALUES(?,?,?)`;
+
+    //암호화된 비밀번호와 salt값을 같이 DB에 저장 
+    const salt = crypto.randomBytes(10).toString('base64');
+    const hashPassword = crypto.pbkdf2Sync(password, salt, 10000, 10,'sha512').toString('base64');
+
+    const values =[email,hashPassword,salt];
+    conn.query(sql,values,
+        (err,results,fields)=>{
+            if(err){
+                console.log(err);
+                return res.status(StatusCodes.BAD_REQUEST).end();
+            }
+            return res.status(StatusCodes.CREATED).json(results);
+        })
+};
+
+const login = (req,res)=>{
+    let {email,password}=req.body;
+    const sql = `SELECT * FROM users WHERE email=?`;
+    const values = [email,password];
+    conn.query(sql,values,
+        (err,results,fields)=>{
+            if(err){
+                console.log(err);
+                return res.status(StatusCodes.BAD_REQUEST).end();
+            }
+            const loginUser=results[0];
+            //salt값 꺼내서 비밀번호를 암호화하고 디비 비밀번호랑비교
+            const hashPassword = crypto.pbkdf2Sync(password, loginUser.salt, 10000, 10,'sha512').toString('base64');
+           
+            if(loginUser&&loginUser.password==hashPassword){
+                const token = jwt.sign({
+                    email:loginUser.email,
+                    name:loginUser.name
+                },process.env.PRIVATE_KEY,{
+                    expiresIn:'5m',
+                    issuer:"choims"
+                });      
+                res.cookie("token",token,{
+                    httpOnly:true
+                });
+                console.log(token);
+                return res.status(StatusCodes.CREATED).json({
+                    message:`${loginUser.email}님 로그인 되었습니다.`
+                });
+            }else{
+                return res.status(StatusCodes.UNAUTHORIZED).end();
+            }
+        })
+};
+const passwordResetRequest = (req,res)=>{
+    const {email} = req.body;
+    let sql ='SELECT * FROM users WHERE email=?';
+    conn.query(sql,email,
+        (err,results)=>{
+            if(err){
+                console.log(err);
+                return res.status(StatusCodes.BAD_REQUEST).end();
+            }
+
+            const user = results[0];
+            if(user){
+                return res.status(StatusCodes.OK).json({
+                    email:email
+                })
+            }else{
+                return res.status(StatusCodes.UNAUTHORIZED).end();
+            }
+            
+        })
+};
+const passwordReset = (req,res)=>{
+    const {email,password} = req.body;
+     
+    const sql = "UPDATE users SET password=? , salt=? WHERE email=?";
+    const salt = crypto.randomBytes(10).toString('base64');
+    const hashPassword = crypto.pbkdf2Sync(password, salt, 10000, 10,'sha512').toString('base64');
+    let values =[hashPassword,salt,email];
+    conn.query(sql,values,
+        (err,results)=>{
+            if(err){
+                console.log(err);
+                return res.status(StatusCodes.BAD_REQUEST).end();
+            }
+            
+            if(results.affectedRows==0)
+                return res.status(StatusCodes.BAD_REQUEST).end();
+            else   
+                return res.status(StatusCodes.OK).json(results);
+        })
+};
+
+module.exports={
+    join,
+    login,
+    passwordResetRequest,
+    passwordReset
+};
--- routes/users.js
+++ routes/users.js
@@ -1,25 +1,37 @@
-const express = require('express');
+const express = require('express'); //express 모듈
 const router = express.Router();
+const conn = require('../mariadb'); //db모듈
+const {body,validationResult}= require('express-validator');
 
+const {
+    join,
+    login,
+    passwordResetRequest,
+    passwordReset
+    } = require('../controller/UserController');
 router.use(express.json());
 
+const validate = (req,res,next)=>{
+    const err = validationResult(req);
+    if(err.isEmpty()){
+        return next();
+    }
+    else{
+        return res.status(400).json(err.array());
+    }
+}
+
 //회원가입
-router.post('/join',(req,res)=>{
-    res.status(201).json({
-        message:"회원가입"
-    })
-});
-//로그인
-router.post('/login',(req,res)=>{
-    res.json("로그인")
-});
-//비밀번호 초기화 요청
-router.post('/reset',(req,res)=>{
-    res.json("비밀번호 초기화 요청")
-}) ;
-//비밀번호 초기화 
-router.put('/reset',(req,res)=>{
-    res.json("비밀번호 초기화")
-});
+router.post('/join',
+    [
+        body('email').notEmpty().isEmail().withMessage('이메일확인필요'),
+        body('password').notEmpty().isString().withMessage('비밀번호 확인필요'),
+        validate
+    ]
+    ,join);
+
+router.post('/login',login);  //로그인
+router.post('/reset',passwordResetRequest); //비밀번호 초기화 요청
+router.put('/reset',passwordReset); //비밀번호 초기화 
 
 module.exports=router
