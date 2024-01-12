86a783c8919ce59bec3bcdd160fa1e3b1a476864
choims <chlaudtn0257@gmail.com>
2024-01-12 21:24:22 +0900

    refactor:좋아요 추가/삭제 jwt토큰인증추가

--- controller/LikeController.js
+++ controller/LikeController.js
@@ -1,37 +1,45 @@
 const conn = require("../mariadb");
-const {StatusCodes}= require('http-status-codes');
+const { StatusCodes } = require("http-status-codes");
+const jwt = require("jsonwebtoken");
 
-const addLike=(req,res)=>{
-    const {id}=req.params;
-    const {user_id}=req.body;
-    const sql = `INSERT INTO likes VALUES(?,?)`;
-    let values=[user_id, Number(id)];
-    conn.query(sql,values,
-        (err,results,fileds)=>{
-            if(err){
-                console.log(err);
-                return res.status(StatusCodes.BAD_REQUEST).end();
-            }
-            return res.status(StatusCodes.CREATED).json(results);
-        })
+const addLike = (req, res) => {
+	const bookId = req.params.id;
+
+	const decodedJwt = decodeJwt(req);
+
+	const sql = `INSERT INTO likes VALUES(?,?)`;
+	let values = [decodedJwt.id, Number(bookId)];
+	conn.query(sql, values, (err, results, fileds) => {
+		if (err) {
+			console.log(err);
+			return res.status(StatusCodes.BAD_REQUEST).end();
+		}
+		return res.status(StatusCodes.CREATED).json(results);
+	});
+};
+
+const removeLike = (req, res) => {
+	const bookId = req.params.id;
+
+	const decodedJwt = decodeJwt(req);
+
+	const sql = `DELETE FROM likes WHERE user_id= ? AND liked_book_id=?`;
+	let values = [decodedJwt.id, Number(bookId)];
+	conn.query(sql, values, (err, results, fields) => {
+		if (err) {
+			console.log(err);
+			return res.status(StatusCodes.BAD_REQUEST).end();
+		}
+		return res.status(StatusCodes.OK).json(results);
+	});
+};
+
+function decodeJwt(req) {
+	const receivedJwt = req.headers["authorization"];
+	return jwt.verify(receivedJwt, process.env.PRIVATE_KEY);
 }
 
-const removeLike = (req,res)=>{
-    const {id} = req.params;
-    const {user_id} = req.body;
-    const sql = `DELETE FROM likes WHERE user_id= ? AND liked_book_id=?`;
-    let value = [user_id,Number(id)]
-    conn.query(sql,value,
-        (err,results,fields)=>{
-            if(err){
-                console.log(err);
-                return res.status(StatusCodes.BAD_REQUEST).end();
-            }
-            return res.status(StatusCodes.OK).json(results);
-        })
-}
-
-module.exports={
-    addLike,
-    removeLike
-}
+module.exports = {
+	addLike,
+	removeLike,
+};
